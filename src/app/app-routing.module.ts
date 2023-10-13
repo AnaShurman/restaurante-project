ac91cabd96415e940406ce44ff1603bb473a9b4a
Ana Beatriz <anashurman@outlook.com>
2023-10-12 21:16:57 -0300

    Implemented routes, styles, and packages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from '../app/app.component';
+import {ListProductComponent} from '../app/list-product/list-product.component';
+import {DashBoardPedidosComponent} from '../app/dash-board-pedidos/dash-board-pedidos.component';
+import {AddUserComponent} from '../app/add-user/add-user.component';
 
-const routes: Routes = [];
 
+const routes: Routes = [
+  { path: '', component: AppComponent , pathMatch: 'full'  },
+  { path: 'produtos', component: ListProductComponent , pathMatch: 'full'},
+  { path: 'pedidos', component: DashBoardPedidosComponent , pathMatch: 'full'},
+  { path: 'usuario', component: AddUserComponent , pathMatch: 'full'},
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
