5b306c6031e30de9fedecf128cd95694ca1f9aae
Ana Beatriz <anashurman@outlook.com>
2023-12-07 11:38:45 -0300

    updated pedidos page

--- src/app/dash-board-orders/dash-board-orders.component.ts
+++ src/app/dash-board-orders/dash-board-orders.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { DashboardPedidosService } from '../services/dashboard/dashboard-pedidos.service'
 import { Product } from '../services/product'
+import { NgZone } from '@angular/core';
+
 @Component({
   selector: 'app-dash-board-orders',
   templateUrl: './dash-board-orders.component.html',
@@ -11,59 +13,45 @@ export class DashBoardOrdersComponent implements OnInit {
   sourceProducts!: Product[];
   products: any
   targetProducts!: Product[];
-
   fineshed!: Product[];
   col1Items: string[] = ['Item 1', 'Item 2', 'Item 3'];
   col2Items: string[] = [];
   col3Items: string[] = [];
-  aguardando: any;
-  preparando: any;
-  pronto: any;
-  constructor(private DashboardPedidosService: DashboardPedidosService, private cdr: ChangeDetectorRef
+  aguardando: any[] = []; teste: any;
+  preparando: any[] = [];
+  pronto: any[] = [];
+  
+  constructor(private DashboardPedidosService: DashboardPedidosService, private cdr: ChangeDetectorRef, private zone: NgZone,
   ) { }
 
-  async data(){
+  async data() {
     this.sourceProducts = await this.DashboardPedidosService.getProductsMini();
-    console.log(this.sourceProducts)
     this.aguardando = this.sourceProducts.filter(item => item.status === 'Aguardando');
     this.preparando = this.sourceProducts.filter(item => item.status === 'Preparando');
     this.pronto = this.sourceProducts.filter(item => item.status === 'Pronto');
   }
   async ngOnInit() {
-this.data()
-    console.log(this.aguardando);
+    this.data()
     this.cdr.markForCheck();
     this.targetProducts = [];
   }
 
-  onItemMove(event: any) {
-    // Lógica para atualizar os outros p-pickList com base no evento
-    // Exemplo: você pode mover os itens entre as listas ou realizar outras ações necessárias.
-    console.log('Item movido:', event);
-  }
-
   moveItem(item: string, targetColumn: string) {
     if (targetColumn === 'col1') {
-      this.col1Items.push(item);
+      console.log(this.col1Items)
     } else if (targetColumn === 'col2') {
       this.col2Items.push(item);
-    } else if (targetColumn === 'col3') {
-      this.col3Items.push(item);
     }
 
     // Remove o item da coluna de origem
     this.col1Items = this.col1Items.filter(i => i !== item);
     this.col2Items = this.col2Items.filter(i => i !== item);
-    this.col3Items = this.col3Items.filter(i => i !== item);
   }
 
-  preparar(aguardando: any){
-    aguardando.status = "Preparando"
-    this.preparando.push(this.aguardando[0])
-    this.aguardando = this.sourceProducts.filter(item => item.status === 'Aguardando');
-    console.log(this.preparando)
-
-
+  async preparar(aguardando: any) {
+      aguardando.status = "Preparando";
+      this.preparando.push(aguardando)
+      this.aguardando = this.sourceProducts.filter(item => item.status === 'Aguardando');
   }
+
 }
-
